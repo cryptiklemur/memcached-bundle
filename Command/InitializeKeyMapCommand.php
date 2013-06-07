c63eb11fca4f65cd3e00ddb3d3676af285270dca
Aaron Scherer <aequasi@gmail.com>
2013-06-07 13:00:00 -0700

    Bumping version, adding debug

--- Command/InitializeKeyMapCommand.php
+++ Command/InitializeKeyMapCommand.php
@@ -66,7 +66,7 @@ INDEX (`insert_date`)
 SQL;
 			
 			$output->writeln( "Attempting to create `memcached_key_map` table" );
-			
+			$output->writeln( $sql );
 			$connection->executeQuery( $sql );
 
 		} catch( ServiceNotFoundException $e ) {
