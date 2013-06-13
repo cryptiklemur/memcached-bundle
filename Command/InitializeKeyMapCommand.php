ec11726cf6584a93d607b0e5fdfaf40bbb9e8398
Aaron Scherer <aequasi@gmail.com>
2013-06-13 09:26:08 -0700

    Adding keyMapInfo

--- Command/InitializeKeyMapCommand.php
+++ Command/InitializeKeyMapCommand.php
@@ -55,7 +55,7 @@ CREATE TABLE IF NOT EXISTS `memcached_key_map` (
 `id` BIGINT(32) UNSIGNED NOT NULL AUTO_INCREMENT,
 `cache_key` VARCHAR(255) NOT NULL,
 `category` VARCHAR(255),
-`description` VARCHAR(1024),
+`description` VARCHAR(2048),
 `memory_size` BIGINT(32) UNSIGNED,
 `lifeTime` INT(11) UNSIGNED,
 `expiration` DATETIME,
